b6289356bc1dd43ebad41e73a1fc49e5e23ae6b7
yixiatros <alexandrosdimitriadis02@gmail.com>
2023-07-20 17:14:27 +0300

    v0.0

---
+++ src/main/java/com/omada5/ergasia_omadas_5/ErgasiaOmadas5Application.java
@@ -0,0 +1,13 @@
+package com.omada5.ergasia_omadas_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgasiaOmadas5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErgasiaOmadas5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/omada5/ergasia_omadas_5/ErgasiaOmadas5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omada5.ergasia_omadas_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgasiaOmadas5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
